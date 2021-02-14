c7278e2bec73659463b1367e2e998fef32bc09dc
makibishi <makibishi0212@gmail.com>
2021-02-15 02:56:44 +0900

    画面をリサイズ

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use bevy::prelude::*;
 
+const UNIT_WIDTH: u32 = 40;
+const UNIT_HEIGHT: u32 = 40;
+
+const X_LENGTH: u32 = 10;
+const Y_LENGTH: u32 = 18;
+
+const SCREEN_WIDTH: u32 = UNIT_WIDTH * X_LENGTH;
+const SCREEN_HEIGHT: u32 = UNIT_HEIGHT * Y_LENGTH;
+
 fn main() {
     App::build()
         .add_resource(WindowDescriptor {
             title: "Tetris".to_string(),
-            width: 500.,
-            height: 500.,
+            width: SCREEN_WIDTH as f32,
+            height: SCREEN_HEIGHT as f32,
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
