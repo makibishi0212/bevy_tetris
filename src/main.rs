84000f25640432352f8e35b9fada450cd03fd490
makibishi <makibishi0212@gmail.com>
2021-02-19 03:54:15 +0900

    矩形の表示

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,17 @@ const Y_LENGTH: u32 = 18;
 const SCREEN_WIDTH: u32 = UNIT_WIDTH * X_LENGTH;
 const SCREEN_HEIGHT: u32 = UNIT_HEIGHT * Y_LENGTH;
 
+fn setup(commands: &mut Commands) {
+    commands.spawn(Camera2dBundle::default());
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite::new(Vec2::new(20.0, 20.0)),
+            ..Default::default()
+        })
+        .current_entity()
+        .unwrap();
+}
+
 fn main() {
     App::build()
         .add_resource(WindowDescriptor {
@@ -18,5 +29,6 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
         .run();
 }
