5a86775f61bc09f1d9e6745af61049084d2a24e5
makibishi <makibishi0212@gmail.com>
2021-02-22 18:13:48 +0900

    Positionコンポーネントの導入

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,35 @@ const Y_LENGTH: u32 = 18;
 const SCREEN_WIDTH: u32 = UNIT_WIDTH * X_LENGTH;
 const SCREEN_HEIGHT: u32 = UNIT_HEIGHT * Y_LENGTH;
 
+struct Position {
+    x: i32,
+    y: i32,
+}
+
 fn setup(commands: &mut Commands) {
     commands.spawn(Camera2dBundle::default());
     commands
         .spawn(SpriteBundle {
-            sprite: Sprite::new(Vec2::new(20.0, 20.0)),
             ..Default::default()
         })
+        .with(Position { x: 1, y: 5 })
         .current_entity()
         .unwrap();
+}
+
+fn position_transform(mut position_query: Query<(&Position, &mut Transform, &mut Sprite)>) {
+    let origin_x = UNIT_WIDTH as i32 / 2 - SCREEN_WIDTH as i32 / 2;
+    let origin_y = UNIT_HEIGHT as i32 / 2 - SCREEN_HEIGHT as i32 / 2;
+    position_query
+        .iter_mut()
+        .for_each(|(pos, mut transform, mut sprite)| {
+            transform.translation = Vec3::new(
+                (origin_x + pos.x as i32 * UNIT_WIDTH as i32) as f32,
+                (origin_y + pos.y as i32 * UNIT_HEIGHT as i32) as f32,
+                0.0,
+            );
+            sprite.size = Vec2::new(UNIT_WIDTH as f32, UNIT_HEIGHT as f32)
+        });
 }
 
 fn main() {
@@ -30,5 +50,6 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_system(position_transform.system())
         .run();
 }
